4375c17e34c0bd25003bbf622a2b962def81202d
Amit <amit.sharma.sf@outlook.com>
2017-08-23 21:39:01 +0530

    Run historical data for one instrument at a time

--- bin/add_historical_data.py
+++ bin/add_historical_data.py
@@ -15,15 +15,22 @@ def parse_options():
                   help="Read and update file")
 	parser.add_option("-l", "--lookback",
                   help="Enter date lookback example 5d, 1Y", default="1Y")
+	parser.add_option("-i", "--instrument",
+                  help="Enter the instrument ticker")
 
 	return parser.parse_args()
 
 def validate_options(options):
 	opts = dict()
 	if(options.file is None or not os.path.isfile(options.file)):
-		log.error("Enter Valid Filename")
+		log.critical("Enter Valid Filename")
 	else:
 		opts['file'] = options.file
+
+	if(options.instrument is None):
+		log.critical("Enter Valid Instrument")
+	else:
+		opts['instrument'] = options.instrument
 
 	opts['lookback'] = options.lookback
 
@@ -37,6 +44,10 @@ def main(options):
 	provider = Provider(log)
 
 	for stock in univ.universe():
+		if(stock['name'] != parsed_options['instrument']):
+			continue
+
+		log.info("Processing: " + stock['name'])
 		price = provider.get_last_trade_data(stock['name'], stock['exchange'], parsed_options['lookback'])
 		instruments = Instruments(log, price.to_dict())
 
